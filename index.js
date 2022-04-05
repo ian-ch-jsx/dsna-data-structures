74c96af2b5e9979e0ebd025a60a4e949d058b71b
Ian Christiansen <ianc.songdog@gmail.com>
2022-04-05 15:34:50 -0700

    Binary Tree challenge

--- index.js
+++ index.js
@@ -87,4 +87,47 @@ class Queue {
 //
 //
 //
-// ***** CHECK SYNTAX *****
+// ***** BINARY NODE ADD *****
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  add(value) {
+    //create a new node
+    const newNode = new Node(value);
+    //if root does not exist, set newNode to root
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (current) {
+      //if the current root value is higher than input node, create left node if left node does not already exist
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        //else add to the right
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+}
+const B = new BinaryTreeNode("B");
+const A = new BinaryTreeNode("A");
+const C = new BinaryTreeNode("C");
+const D = new BinaryTreeNode("D");
+B.add(A);
+B.add(D);
+B.add(C);
+console.log(C);
