0c9697bc1e467ed3a9a3a5446680dec6fecee70a
Ian Christiansen <ianc.songdog@gmail.com>
2022-04-05 15:36:12 -0700

    corrected new Node error

--- index.js
+++ index.js
@@ -1,92 +1,92 @@
-// ***** STACK *****
-class Stack {
-  constructor() {
-    this.items = [];
-  }
+// // ***** STACK *****
+// class Stack {
+//   constructor() {
+//     this.items = [];
+//   }
 
-  push(element) {
-    this.items.push(element);
-  }
+//   push(element) {
+//     this.items.push(element);
+//   }
 
-  pop() {
-    if (this.items.length == 0) return -1;
-    return this.items.pop();
-  }
+//   pop() {
+//     if (this.items.length == 0) return -1;
+//     return this.items.pop();
+//   }
 
-  peek() {
-    return this.items[this.items.length - 1];
-  }
-}
-// const stack = new Stack();
-// stack.push("fox");
-// stack.push("goose");
-// stack.push("lizard");
-// console.log(stack.pop());
-// console.log(stack.peek());
-//
-//
-//
-// ***** QUEUE *****
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+//   peek() {
+//     return this.items[this.items.length - 1];
+//   }
+// }
+// // const stack = new Stack();
+// // stack.push("fox");
+// // stack.push("goose");
+// // stack.push("lizard");
+// // console.log(stack.pop());
+// // console.log(stack.peek());
+// //
+// //
+// //
+// // ***** QUEUE *****
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
 
-class Queue {
-  constructor() {
-    this.length = 0;
-    this.start = null;
-    this.end = null;
-  }
+// class Queue {
+//   constructor() {
+//     this.length = 0;
+//     this.start = null;
+//     this.end = null;
+//   }
 
-  enqueue(value) {
-    const newNode = new Node(value);
+//   enqueue(value) {
+//     const newNode = new Node(value);
 
-    if (!this.length) {
-      this.start = newNode;
-      this.end = newNode;
-    } else {
-      this.end.next = newNode;
-      this.end = newNode;
-    }
+//     if (!this.length) {
+//       this.start = newNode;
+//       this.end = newNode;
+//     } else {
+//       this.end.next = newNode;
+//       this.end = newNode;
+//     }
 
-    this.length += 1;
-    return newNode;
-  }
-  dequeue() {
-    if (!this.length) {
-      return null;
-    } else {
-      const nodeToRemove = this.start;
+//     this.length += 1;
+//     return newNode;
+//   }
+//   dequeue() {
+//     if (!this.length) {
+//       return null;
+//     } else {
+//       const nodeToRemove = this.start;
 
-      this.start = this.start.next;
+//       this.start = this.start.next;
 
-      nodeToRemove.next = null;
+//       nodeToRemove.next = null;
 
-      if (this.length === 1) {
-        this.end = null;
-      }
+//       if (this.length === 1) {
+//         this.end = null;
+//       }
 
-      this.length -= 1;
+//       this.length -= 1;
 
-      return nodeToRemove;
-    }
-  }
-  hasNext() {
-    return !this.nextVal;
-  }
-}
-// const queue = new Queue();
-// queue.enqueue("fox");
-// queue.enqueue("goose");
-// queue.enqueue("lizard");
-// console.log(queue.dequeue());
-// console.log(queue.hasNext());
-//
-//
-//
+//       return nodeToRemove;
+//     }
+//   }
+//   hasNext() {
+//     return !this.nextVal;
+//   }
+// }
+// // const queue = new Queue();
+// // queue.enqueue("fox");
+// // queue.enqueue("goose");
+// // queue.enqueue("lizard");
+// // console.log(queue.dequeue());
+// // console.log(queue.hasNext());
+// //
+// //
+// //
 // ***** BINARY NODE ADD *****
 class BinaryTreeNode {
   constructor(value) {
@@ -97,7 +97,7 @@ class BinaryTreeNode {
 
   add(value) {
     //create a new node
-    const newNode = new Node(value);
+    const newNode = new BinaryTreeNode(value);
     //if root does not exist, set newNode to root
     if (this.root === null) {
       this.root = newNode;
