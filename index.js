bd5bd896b8ad6af65f8a20afc59cfd3b6de78d11
Ian Christiansen <ianc.songdog@gmail.com>
2022-04-05 15:41:04 -0700

    final notes on binary tree add challenge

--- index.js
+++ index.js
@@ -1,92 +1,3 @@
-// // ***** STACK *****
-// class Stack {
-//   constructor() {
-//     this.items = [];
-//   }
-
-//   push(element) {
-//     this.items.push(element);
-//   }
-
-//   pop() {
-//     if (this.items.length == 0) return -1;
-//     return this.items.pop();
-//   }
-
-//   peek() {
-//     return this.items[this.items.length - 1];
-//   }
-// }
-// // const stack = new Stack();
-// // stack.push("fox");
-// // stack.push("goose");
-// // stack.push("lizard");
-// // console.log(stack.pop());
-// // console.log(stack.peek());
-// //
-// //
-// //
-// // ***** QUEUE *****
-// class Node {
-//   constructor(value) {
-//     this.value = value;
-//     this.next = null;
-//   }
-// }
-
-// class Queue {
-//   constructor() {
-//     this.length = 0;
-//     this.start = null;
-//     this.end = null;
-//   }
-
-//   enqueue(value) {
-//     const newNode = new Node(value);
-
-//     if (!this.length) {
-//       this.start = newNode;
-//       this.end = newNode;
-//     } else {
-//       this.end.next = newNode;
-//       this.end = newNode;
-//     }
-
-//     this.length += 1;
-//     return newNode;
-//   }
-//   dequeue() {
-//     if (!this.length) {
-//       return null;
-//     } else {
-//       const nodeToRemove = this.start;
-
-//       this.start = this.start.next;
-
-//       nodeToRemove.next = null;
-
-//       if (this.length === 1) {
-//         this.end = null;
-//       }
-
-//       this.length -= 1;
-
-//       return nodeToRemove;
-//     }
-//   }
-//   hasNext() {
-//     return !this.nextVal;
-//   }
-// }
-// // const queue = new Queue();
-// // queue.enqueue("fox");
-// // queue.enqueue("goose");
-// // queue.enqueue("lizard");
-// // console.log(queue.dequeue());
-// // console.log(queue.hasNext());
-// //
-// //
-// //
 // ***** BINARY NODE ADD *****
 class BinaryTreeNode {
   constructor(value) {
@@ -130,4 +41,6 @@ const D = new BinaryTreeNode("D");
 B.add(A);
 B.add(D);
 B.add(C);
-console.log(C);
+
+// this still returns left and right as null. we looked this code up on stackoverflow, etc, and it makes sense there AFAIK but I'm not sure how it determines
+// left value vs right
