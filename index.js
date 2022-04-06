a42ba6e3c855eddb26addec188976af221543b1f
Ian Christiansen <ianc.songdog@gmail.com>
2022-04-05 17:07:11 -0700

    working on making BinaryTreeMode recursive

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// ***** BINARY NODE ADD *****
 class BinaryTreeNode {
   constructor(value) {
     this.value = value;
@@ -6,42 +5,12 @@ class BinaryTreeNode {
     this.right = null;
   }
 
-  add(value) {
-    //create a new node
-    const newNode = new BinaryTreeNode(value);
-    //if root does not exist, set newNode to root
-    if (this.root === null) {
-      this.root = newNode;
-      return this;
+  add(node) {
+    if (!node || node.value === this.value) {
+      return;
     }
-    let current = this.root;
-    while (current) {
-      //if the current root value is higher than input node, create left node if left node does not already exist
-      if (value < current.value) {
-        if (current.left === null) {
-          current.left = newNode;
-          return this;
-        }
-        current = current.left;
-      } else {
-        //else add to the right
-        if (current.right === null) {
-          current.right = newNode;
-          return this;
-        }
-        current = current.right;
-      }
+    if (node.value > this.value) {
+      node.right = add(node.right, value);
     }
   }
 }
-const B = new BinaryTreeNode("B");
-const A = new BinaryTreeNode("A");
-const C = new BinaryTreeNode("C");
-const D = new BinaryTreeNode("D");
-B.add(A);
-B.add(D);
-B.add(C);
-
-// this still returns left and right as null. we looked this code up on stackoverflow, etc, and it makes sense there AFAIK but I'm not sure how it determines
-// left value vs right
-// and now I think we were working on the wrong one, which might explain why I'm lost.
---
+++ index2.js
@@ -0,0 +1,49 @@
+// ***** BINARY NODE ADD *****
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  add(value) {
+    //create a new node
+    const newNode = new BinaryTreeNode(value);
+    //if root does not exist, set newNode to root
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (current) {
+      //if the current root value is higher than input node, create left node if left node does not already exist
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        //else add to the right
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+}
+const B = new BinaryTreeNode("B");
+const A = new BinaryTreeNode("A");
+const C = new BinaryTreeNode("C");
+const D = new BinaryTreeNode("D");
+B.add(A);
+B.add(D);
+B.add(C);
+
+console.log("A", A);
+
+// this still returns left and right as null. we looked this code up on stackoverflow, etc, and it makes sense there AFAIK but I'm not sure how it determines
+// left value vs right
+// and now I think we were working on the wrong one, which might explain why I'm lost.
