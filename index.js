c7eb619acb457789fbd1b4ee91b28dfe50893916
Ian Christiansen <ianc.songdog@gmail.com>
2022-04-05 17:32:57 -0700

    binary tree node

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { add } = require("nodemon/lib/rules");
+
 class BinaryTreeNode {
   constructor(value) {
     this.value = value;
@@ -6,11 +8,28 @@ class BinaryTreeNode {
   }
 
   add(node) {
-    if (!node || node.value === this.value) {
-      return;
-    }
-    if (node.value > this.value) {
-      node.right = add(node.right, value);
+    // if value is less than current value
+    if (node.value < this.value) {
+      //if no left node, make this the left node
+      if (!this.left) this.left = node;
+      //otherwise add a new node and make this the new node value?
+      else this.left.add(node);
+    } else {
+      //if no right, make this the right.
+      if (!this.right) this.right = node;
+      //otherwise add a new node with this value
+      else this.right.add(node);
     }
   }
 }
+
+const B = new BinaryTreeNode("B");
+const A = new BinaryTreeNode("A");
+const C = new BinaryTreeNode("C");
+const D = new BinaryTreeNode("D");
+
+B.add(A);
+B.add(D);
+B.add(C);
+
+console.log(B);
